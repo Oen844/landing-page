f80a041230e5a65b76b26a92bc552e49ae5a1d4b
Pau Egea <papau84@gmail.com>
2022-04-15 00:44:33 +0200

    modal works

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const modal = document.querySelector(".modal");
-const close1 = document.querySelector(".modal__container--close");
+const close1 = document.querySelector(".modal__content--close");
 
 button1.addEventListener("click", () => {
   modal.classList.remove("hidden");
