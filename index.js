7efa54a7696a9fbabdab70a51e03bc717b23b9b1
Pau Egea <papau84@gmail.com>
2022-04-14 22:30:36 +0200

    modal

---
+++ index.js
@@ -0,0 +1,23 @@
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const modal = document.querySelector(".modal");
+const close1 = document.querySelector(".modal__container--close");
+
+button1.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+button2.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+button3.addEventListener("click", () => {
+  modal.classList.remove("hidden");
+  modal.classList.add("visible");
+});
+
+close1.addEventListener("click", () => {
+  modal.classList.remove("visible");
+  modal.classList.add("hidden");
+});
